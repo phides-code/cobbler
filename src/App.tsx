934269b92855f362293ecbcc8d73ed8d8ef74c61
Philippe DeSousa <phil.desousa@gmail.com>
2024-04-06 22:21:07 -0400

    remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import FileUpload from '../FileUpload';
 import AddRecipe from './AddRecipe';
 import './App.css';
 
 const App = () => {
     return (
         <div className='App'>
-            {/* <FileUpload /> */}
             <AddRecipe />
         </div>
     );
