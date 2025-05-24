1de59e61b26e115721a2a3e07c8efa66d62894ce
Philippe DeSousa <phil.desousa@gmail.com>
2025-05-24 16:11:09 -0400

    improve typeahead styling

--- src/components/TypeaheadSearch.tsx
+++ src/components/TypeaheadSearch.tsx
@@ -3,6 +3,7 @@ import { useSearchRecipesQuery } from '../features/recipes/recipesApiSlice';
 import { useDebounce } from '../app/hooks';
 import { Link } from 'react-router';
 import { truncateText } from '../utils/utils';
+import type { Recipe } from '../types';
 
 const TypeaheadSearch = () => {
     const [query, setQuery] = useState('');
@@ -11,28 +12,58 @@ const TypeaheadSearch = () => {
         skip: !debouncedQuery,
     });
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Escape') {
+            setQuery('');
+        }
+    };
+
+    const recipes = data?.data as Recipe[];
+
     return (
-        <div>
+        <div className='typeahead-search-container'>
             <input
                 type='text'
+                className='typeahead-search-input'
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
-                placeholder='Type to search...'
+                onKeyDown={handleKeyDown}
+                placeholder='Search recipes...'
             />
-            {isLoading && <p>Loading...</p>}
-            {error && <p>Error fetching results.</p>}
-            <ul>
-                {data?.data?.map((recipe) => (
-                    <li key={recipe.id}>
-                        <div>
-                            <Link to={`/recipe/${recipe.id}`}>
-                                {recipe.title}
-                            </Link>
+            {query && (
+                <div className='typeahead-search-results'>
+                    {isLoading && (
+                        <div className='typeahead-search-loading'>
+                            Searching...
                         </div>
-                        <div>{truncateText(recipe.description, 50)}</div>
-                    </li>
-                ))}
-            </ul>
+                    )}
+                    {error && (
+                        <div className='typeahead-search-error'>
+                            Error fetching results.
+                        </div>
+                    )}
+                    {!isLoading && !error && recipes.length === 0 && (
+                        <div className='typeahead-search-no-results'>
+                            No recipes found.
+                        </div>
+                    )}
+                    {recipes.map((recipe) => (
+                        <Link
+                            to={`/recipe/${recipe.id}`}
+                            className='typeahead-search-result-item'
+                            key={recipe.id}
+                            onClick={() => setQuery('')}
+                        >
+                            <div className='typeahead-search-result-title'>
+                                {recipe.title}
+                            </div>
+                            <div className='typeahead-search-result-description'>
+                                {truncateText(recipe.description, 50)}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
