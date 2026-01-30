bfeb08ea06a6e1daddc5c3af0c47aede11324d2e
Philippe DeSousa <phil.desousa@gmail.com>
2026-01-30 13:45:53 -0500

    remove commented code

--- src/components/TypeaheadSearch.tsx
+++ src/components/TypeaheadSearch.tsx
@@ -44,11 +44,6 @@ const TypeaheadSearch = () => {
                             Searching...
                         </div>
                     )}
-                    {/* {error && (
-                        <div className='typeahead-search-error'>
-                            Error fetching results.
-                        </div>
-                    )} */}
                     {!isLoading &&
                         !error &&
                         recipes &&
